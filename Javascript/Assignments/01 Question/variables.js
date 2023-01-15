260c0a3631a9b9a5f63ce1bffe20290b12dd6f49
Angshuman <angshumanbhattacharjee3@gmail.com>
2023-01-15 23:04:43 +0530

    JS Assignment

---
+++ Javascript/Assignments/01 Question/variables.js
@@ -0,0 +1,9 @@
+let name='Angshuman';
+let enroll=true;
+let status=undefined;
+let remarks=null;
+
+console.log(typeof(name));
+console.log(typeof(enroll))
+console.log(typeof(status))
+console.log(typeof(remarks))
