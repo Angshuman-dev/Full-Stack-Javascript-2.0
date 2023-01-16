d40b37d3fbabd82299c3f39a03d50bff07479839
Angshuman <angshumanbhattacharjee3@gmail.com>
2023-01-16 23:36:15 +0530

    js assignment question 2

---
+++ Javascript/Assignments/02 Question/Interpolation.js
@@ -0,0 +1,7 @@
+let first_name='Angshuman';
+let last_name='Bhattacharjee';
+let marital_status='Single';
+let country='India';
+let age=29
+
+console.log(`Name : ${first_name} ${last_name}\nMarital Status: ${marital_status}\nCountry: ${country}\nAge: ${age}`)
