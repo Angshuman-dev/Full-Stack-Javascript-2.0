6dc9ad0935cbc82ec15a3d75197e234d17debc73
Angshuman <angshumanbhattacharjee3@gmail.com>
2023-01-25 23:50:39 +0530

    js assignment question 11 added

---
+++ Javascript/Assignments/11 Question/solution.js
@@ -0,0 +1,10 @@
+let date= new Date;
+
+console.log('Year: ',date.getFullYear())
+console.log('Month: ',(date.getMonth())+1)
+console.log('Date:',date.getDate())
+console.log('Date:',date.getDay())
+console.log('Hours:',date.getHours())
+console.log('Minutes:',date.getMinutes())
+console.log('Milliseconds elapsed:',date.getTime())
+
