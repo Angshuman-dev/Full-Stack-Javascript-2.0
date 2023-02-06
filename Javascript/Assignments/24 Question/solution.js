98fe7aa8589ec00d5b660a05c04f37a37ae0ed18
Angshuman <angshumanbhattacharjee3@gmail.com>
2023-02-06 23:36:58 +0530

    js assignment question 24 added

---
+++ Javascript/Assignments/24 Question/solution.js
@@ -0,0 +1,14 @@
+let even=[];
+let odd=[];
+for (i=0;i<=100;i++){
+    if (i%2==0){
+        even.push(i)
+    }
+    else{
+        odd.push(i)
+    }
+    
+}
+
+console.log(`even :  ${even}`)
+console.log(`odd : ${odd}`)
