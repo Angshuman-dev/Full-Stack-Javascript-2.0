92f9ae6c6657f9b5278adc1787f3cae7638915b4
Angshuman <angshumanbhattacharjee3@gmail.com>
2023-02-09 23:58:02 +0530

    js assignment question 26 added

---
+++ Javascript/Assignments/26 Question/solution.js
@@ -0,0 +1,9 @@
+let number= prompt("Enter a number to find its multiple")
+
+function table(number)
+    {
+        for (i=1;i<=10;i++)
+        {
+            console.log(`${number} * ${i} = ${number*i}`)
+        }
+    }
